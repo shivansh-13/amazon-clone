42045b6e68c9bb68dd9ef99e8e373b4d8ffce88f
shivansh-13 <shivansh01308@gmail.com>
2021-08-02 20:10:26 +0530

    added home and cart pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Header from './Header';   
+import Header from './Header';
+import Cart from './Cart';
+import Home from './Home';
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/cart">
+            <Cart/>
+          </Route>
+
+          <Route path="/">
+            <Home/>
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/Cart.js
+++ src/Cart.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
 function Cart() {
     return (
         <div>
-            
+            hvhvjcjgcjgc
         </div>
     )
 }
 
-export default Cart
+export default Cart;
