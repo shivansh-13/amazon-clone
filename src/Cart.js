7c0d348497c150ffc7630cfd7f9697dde88c9f42
shivansh-13 <shivansh01308@gmail.com>
2021-08-03 17:08:37 +0530

    Added Cart and its components

--- src/Cart.js
+++ src/Cart.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
+import CartItems from './CartItems';
+import CartTotal from './CartTotal';
 
 function Cart() {
     return (
-        <div>
-            cart
-        </div>
+        <Container>
+            <CartItems/>
+            <CartTotal/>
+        </Container>
     )
 }
 
 export default Cart;
+
+const Container = styled.div`
+    display:flex;
+    
+`
