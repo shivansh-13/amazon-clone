b2eb8f325c6b9b3f808ce326c33faebccc94dcc6
shivansh-13 <shivansh01308@gmail.com>
2021-08-05 19:47:11 +0530

    added number format npm

--- src/Cart.js
+++ src/Cart.js
@@ -4,10 +4,26 @@ import CartItems from './CartItems';
 import CartTotal from './CartTotal';
 
 function Cart({ cartItems }) {
+
+    const getTotalPrice = () => {
+        let total = 0;
+        cartItems.forEach((item) => {
+            total += (item.product.price * item.product.quantity)  
+        })
+        return total;
+    }
+
+    const getTotalQuantity = () => {
+        let totalq = 0;
+        cartItems.forEach((item) => {
+            totalq += (item.product.quantity)
+        })
+        return totalq;
+    }
     return (
         <Container>
-            <CartItems cartItems = {cartItems}/>
-            <CartTotal />
+            <CartItems cartItems={cartItems} />
+            <CartTotal getTotalPrice={getTotalPrice} getTotalQuantity={getTotalQuantity} />
         </Container>
     )
 }
@@ -17,5 +33,6 @@ export default Cart;
 const Container = styled.div`
     display:flex;
     margin: 14px 18px 0 18px;
+    align-items:flex-start;
     
 `
